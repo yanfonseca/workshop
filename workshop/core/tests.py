c81643fce47d87d583dabdcc5978b572c0fdde03
Yan <yan.fonseca@gmail.com>
2020-11-22 01:33:16 -0300

    Primeiro teste, encontrado HttpResponse().

--- workshop/core/tests.py
+++ workshop/core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HomeTest(TestCase):
+    
+    def test_get(self):
+        """GET/Must return status code 200"""
+        resp = self.client.get('')
+        self.assertEqual(200, resp.status_code )
