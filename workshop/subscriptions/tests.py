90cd3df1113b9fbd961963e3d7f0167c81dbd691
Yan <yan.fonseca@gmail.com>
2020-11-24 21:25:09 -0300

    Teste do conteúdo do e-mail: email.subject, email.from_email, email.to, email.body. O conteúdo do email.body passado ainda está hardcoded, precisa melhorar.

--- workshop/subscriptions/tests.py
+++ workshop/subscriptions/tests.py
@@ -44,23 +44,77 @@ class SubscribeTestGet(TestCase):
 class SubscribePostValid(TestCase):
     def test_post(self):
         """Valid Post must redirect to /subscribe/"""
-        post_exemple = dict(
+        post_example = dict(
             zip(
                 ('name', 'cpf', 'email', 'phone'),
-                ('John Doe', '12345678901', 'john@teste.com', '(61)99999-9999')
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
             )
         )
 
-        resp = self.client.post('/inscricao/', post_exemple)
+        resp = self.client.post('/inscricao/', post_example)
         self.assertEqual(302, resp.status_code)
 
     def test_send_subscribe(self):
         """Valid POST must send one email"""
-        post_exemple = dict(
+        post_example = dict(
             zip(
                 ('name', 'cpf', 'email', 'phone'),
-                ('John Doe', '12345678901', 'john@teste.com', '(61)99999-9999')
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
             )
         )
-        resp = self.client.post('/inscricao/', post_exemple)
+        self.client.post('/inscricao/', post_example)
         self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_subject_email(self):
+        """Must return a standard subject"""
+        post_example = dict(
+            zip(
+                ('name', 'cpf', 'email', 'phone'),
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
+            )
+        )
+        self.client.post('/inscricao/', post_example)
+        email = mail.outbox[0]
+        expected = 'Formulário preenchido com sucesso'
+        self.assertEqual(expected, email.subject)
+
+    def test_subscription_from_email(self):
+        """Must return the email sender"""
+        post_example = dict(
+            zip(
+                ('name', 'cpf', 'email', 'phone'),
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
+            )
+        )
+        self.client.post('/inscricao/', post_example)
+        email = mail.outbox[0]
+        expected = 'sender@email.com'
+        self.assertEqual(expected, email.from_email)
+
+    def test_subscription_email_to(self):
+        """Must return the email to"""
+        post_example = dict(
+            zip(
+                ('name', 'cpf', 'email', 'phone'),
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
+            )
+        )
+        self.client.post('/inscricao/', post_example)
+        email = mail.outbox[0]
+        expected = ['sender@email.com', 'john@email.com']
+        self.assertEqual(expected, email.to)
+
+    def test_subscription_email_message(self):
+        """Must return the email message with subscriber data"""
+        post_example = dict(
+            zip(
+                ('name', 'cpf', 'email', 'phone'),
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
+            )
+        )
+        self.client.post('/inscricao/', post_example)
+        email = mail.outbox[0]
+        self.assertIn('John Doe', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('john@email.com', email.body)
+        self.assertIn('(61)99999-9999', email.body)
--- workshop/subscriptions/views.py
+++ workshop/subscriptions/views.py
@@ -1,20 +1,35 @@
 from django.core import mail
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+
 from workshop.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
     if request.method == 'POST':
         context = {'form': SubscriptionForm()}
-        subject = 'subject'
-        message = 'mensagem'
-        from_email = 'contato@workshop.com'
-        to_email = 'john@teste.com'
+
+        subject = 'Formulário preenchido com sucesso'
+        body = MENSAGEM #render_to_string('subscriptions/subscription_email.txt')
+        from_email = 'sender@email.com'
+        to_email = 'john@email.com'
         recipient_list = [from_email, to_email]
         mail.send_mail(
-            subject, message,
+            subject, body,
             from_email, recipient_list)
         return redirect('/inscricao/', context)
 
     context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
+
+MENSAGEM = """
+Olá!
+
+Bem vindo ao melhor workshop do mundo!
+
+Os dados informados foram:
+nome: John Doe
+cpf: 12345678901
+Email: john@email.com
+Telefone: (61)99999-9999
+"""
