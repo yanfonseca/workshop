d29a0a4b8ad846b5a34541d4336191637969b71b
Yan <yan.fonseca@gmail.com>
2020-11-25 20:28:01 -0300

    Model criado. A partir daqui é necessário fazer a migração antes de rodar os testes ou usar a flag -n do withoumigrations.

--- workshop/subscriptions/tests/test_model_subscription.py
+++ workshop/subscriptions/tests/test_model_subscription.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
 from workshop.subscriptions.models import Subscription
 
+# From here is necessary to think clearly about migrations
+# (1) You always have to test with the flag -n or (2) You have to migrate first
+
+# (1)
+# python manage.py test -n
+
+# (2)
+# python manage.py makemigrations
+# python manage.py migrate
 
 class SubscriptionModelTest(TestCase):
 
@@ -11,7 +20,7 @@ class SubscriptionModelTest(TestCase):
                 ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
             )
         )
-        obj = Subscription(post_example)
+        obj = Subscription(**post_example)
         obj.save()
 
         self.assertTrue(Subscription.objects.exists())
