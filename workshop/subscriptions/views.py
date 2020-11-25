c7625a41082af3494b33fb190f9e34a6ed4b35c6
Yan <yan.fonseca@gmail.com>
2020-11-24 22:45:27 -0300

    Recebe o que é preenchido no formulário para fazer o post.

--- workshop/subscriptions/views.py
+++ workshop/subscriptions/views.py
@@ -7,25 +7,17 @@ from workshop.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
     if request.method == 'POST':
-        post_example = dict(
-            zip(
-                ('name', 'cpf', 'email', 'phone'),
-                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
-            )
-        )
-
-        context = {'form': SubscriptionForm()}
-
+        form = SubscriptionForm(request.POST)
+        form.full_clean()
         subject = 'Formulário preenchido com sucesso'
-        body = render_to_string('subscriptions/subscription_email.txt', post_example)
+        body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
         from_email = 'sender@email.com'
         to_email = 'john@email.com'
         recipient_list = [from_email, to_email]
         mail.send_mail(
             subject, body,
             from_email, recipient_list)
-        return redirect('/inscricao/', context)
+        return redirect('/inscricao/')
 
     context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
-
