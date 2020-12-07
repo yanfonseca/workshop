450678e733507728528561d912b1e044fcc8fccb
Yan <yan.fonseca@gmail.com>
2020-12-06 23:38:01 -0300

    Validação do cpf

--- workshop/subscriptions/forms.py
+++ workshop/subscriptions/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
+
+def validade_cpf(value):
+    """Validates accepts only one value."""
+    if not value.isdigit():
+        raise ValidationError('CPF precisa ter apenas números', 'digits_cpf')
+
+    if len(value) != 11:
+        raise ValidationError('CPF precisa ter 11 números', 'lenght_cpf')
 
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF')
+    cpf = forms.CharField(label='CPF', validators=[validade_cpf])
     email = forms.EmailField(label='Email')
     phone = forms.CharField(label='Celular')
--- workshop/subscriptions/tests/test_form_subscription.py
+++ workshop/subscriptions/tests/test_form_subscription.py
@@ -5,13 +5,56 @@ from workshop.subscriptions.forms import SubscriptionForm
 # Test object form
 class SubscriptionFormTest(TestCase):
 
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
         """Form must have some fields"""
-        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(self.form.fields))
+        form = SubscriptionForm()
+        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
 
+    def test_cpf_is_digit(self):
+        """"CPF must be digits"""
+        form = self.make_validated_form(cpf='ABCD5678901')
+        # self.assertListEqual(['cpf'], list(form.errors))
+        # msg = 'CPF precisa ter apenas números'
+        # field ='cpf'
+        # self.assertFormErrorMessage(form, field, msg)
+        msg = 'CPF precisa ter apenas números'
+        field ='cpf'
+        code ='digits_cpf'
+        self.assertFormErrorCode(form, field, code)
+
+    def test_cpf_has_11_digits(self):
+        """CPF must have 11 digits"""
+        form = self.make_validated_form(cpf='5678901')
+        #self.assertListEqual(['cpf'], list(form.errors))
+        # msg = 'CPF precisa ter 11 números'
+        # field = 'cpf'
+        # self.assertFormErrorMessage(form, field, msg)
+        field = 'cpf'
+        code = 'lenght_cpf'
+        self.assertFormErrorCode(form, field, code)
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    # def assertFormErrorMessage(self, form, field, msg):
+    #     errors = form.errors
+    #     errors_list = errors[field]
+    #     self.assertListEqual([msg], errors_list)
+
+
+    def make_validated_form(self, **kwargs):
+        valid_example = dict(
+            zip(
+                ('name', 'cpf', 'email', 'phone'),
+                ('John Doe', '12345678901', 'john@email.com', '(61)99999-9999')
+            )
+        )
+        data_example = dict(valid_example, **kwargs)
+        form = SubscriptionForm(data_example)
+        form.is_valid()
+        return form
 # If you want to test form from the page
 
 # from django.test import TestCase
